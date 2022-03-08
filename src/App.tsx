e5f419398d17cd0bb021eeebdee79fc4d14c44d0
ramand <dineshkumar.raman@umusic.com>
2022-03-08 20:08:51 +0530

    Dark & Light themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,106 @@
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { useState } from 'react';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { PaletteMode } from '@mui/material';
+import Box from '@mui/material/Box';
+import { amber, deepOrange, grey } from '@mui/material/colors';
+import CssBaseline from '@mui/material/CssBaseline';
+import IconButton from '@mui/material/IconButton';
+import { createTheme, ThemeProvider, useTheme } from '@mui/material/styles';
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0);
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
+import Counter from './Counter';
+function MyApp() {
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
   return (
-    <>
-      <div className="flex justify-center align-middle">
-        <Typography variant="h3">React and Tailwind with Vitejs!</Typography>
-      </div>
-      <Button variant="contained" onClick={() => setCount((count) => count + 1)}>
-        Click me : {count}
-      </Button>
-    </>
+    <Box
+      sx={{
+        display: 'flex',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        bgcolor: 'background.default',
+        color: 'text.primary',
+        borderRadius: 1,
+        p: 3,
+      }}
+    >
+      {theme.palette.mode} mode
+      <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+        {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+      </IconButton>
+      <Counter />
+    </Box>
   );
 }
 
-export default App;
+const LightThemePalette = {
+  primary: amber,
+  divider: amber[200],
+  text: {
+    primary: grey[900],
+    secondary: grey[800],
+  },
+};
+
+const DarkThemePalette = {
+  primary: deepOrange,
+  divider: deepOrange[700],
+  background: {
+    default: deepOrange[900],
+    paper: deepOrange[900],
+  },
+  text: {
+    primary: '#fff',
+    secondary: grey[500],
+  },
+};
+
+const getDesignTokens = (mode: PaletteMode) => ({
+  typography: {
+    fontFamily: ['Roboto Condensed', 'sans-serif'].join(','),
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 400,
+    fontWeightBold: 700,
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'Roboto Condensed';
+          font-display: swap;
+        }
+      `,
+    },
+  },
+  palette: {
+    mode,
+    ...(mode === 'light' ? LightThemePalette : DarkThemePalette),
+  },
+});
+
+export default function ToggleColorMode() {
+  const [mode, setMode] = React.useState<'light' | 'dark'>('light');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <MyApp />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
---
+++ src/Counter.tsx
@@ -0,0 +1,20 @@
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="flex justify-center align-middle">
+        <Typography variant="h3">React and Tailwind with Vitejs!</Typography>
+      </div>
+      <Button variant="contained" onClick={() => setCount((count) => count + 1)}>
+        Click me : {count}
+      </Button>
+    </>
+  );
+}
+
+export default Counter;
