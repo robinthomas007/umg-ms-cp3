8b71f8dba78dc6ab46729f3f0da4f11e54d385c0
ramand <dineshkumar.raman@umusic.com>
2022-03-08 20:11:30 +0530

    Dark & Light themes

--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,13 @@
 import './index.css';
 
-import CssBaseline from '@mui/material/CssBaseline';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
 
-const theme = createTheme({
-  typography: {
-    fontFamily: ['Roboto Condensed', 'sans-serif'].join(','),
-  },
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: `
-        @font-face {
-          font-family: 'Roboto Condensed';
-          font-style: normal;
-          font-display: swap;
-          font-weight: 400;
-          font-size: 1.5rem;
-        }
-      `,
-    },
-  },
-});
-
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
