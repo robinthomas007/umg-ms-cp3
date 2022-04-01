95e7b3d138930fa2db3041ce5678e36ae528f453
ramand <dineshkumar.raman@umusic.com>
2022-04-01 10:28:47 +0530

    Change logo

--- src/globalComponents/Navbar/components/ElasticLogo.tsx
+++ src/globalComponents/Navbar/components/ElasticLogo.tsx
@@ -1,10 +1,10 @@
 import Box from '@mui/material/Box';
-import logo from 'static/logo.png';
+import logo from 'static/UMG-logo.png';
 
 const ElasticLogo = () => {
   return (
     <Box sx={{ flexGrow: 1, height: '4rem' }}>
-      <img src={logo} alt="logo" style={{ height: '4rem' }} />
+      <img src={logo} alt="logo" style={{ height: '4rem', padding: '0.5rem' }} />
     </Box>
   );
 };
