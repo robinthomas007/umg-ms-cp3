a9302f6e3dd079ddb5b6f9d92c20af1ea6ba6ef5
ramand <dineshkumar.raman@umusic.com>
2022-04-11 15:45:53 +0530

    Eslint - remove unwanted error for typescript & sort table feature

--- src/pages/Landing/components/Table.tsx
+++ src/pages/Landing/components/Table.tsx
@@ -7,33 +7,131 @@ import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
+import TableCell, { TableCellProps } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
 import TableRowSkeleton from 'globalComponents/TableRowSkeleton';
+import { useState } from 'react';
 
-import { useGetUsers } from '../query';
+import { useGetUsers, User } from '../query';
+
+type Order = 'asc' | 'desc';
+
+interface HeadCell {
+  id: keyof User;
+  label: string;
+  align?: TableCellProps['align'];
+  sortable?: boolean;
+}
+
+interface ExtraHeadCell {
+  label: string;
+  align?: TableCellProps['align'];
+  padding?: TableCellProps['padding'];
+}
+
+const headCells: readonly HeadCell[] = [
+  {
+    id: 'name',
+    label: 'Name',
+    sortable: true,
+  },
+  {
+    id: 'username',
+    label: 'Username',
+    sortable: true,
+  },
+  {
+    id: 'email',
+    label: 'Email',
+  },
+  {
+    id: 'phone',
+    label: 'Phone',
+  },
+];
+
+interface EnhancedTableProps {
+  onSort: (key: keyof User) => void;
+  order: Order;
+  orderBy: string;
+
+  extraHeadCells?: readonly ExtraHeadCell[];
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const { order, orderBy, onSort, extraHeadCells } = props;
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox color="primary" />
+        </TableCell>
+        {headCells.map((headCell) => (
+          <TableCell key={headCell.id} align={headCell.align}>
+            {headCell.sortable ? (
+              <TableSortLabel
+                active={orderBy === headCell.id}
+                direction={orderBy === headCell.id ? order : 'asc'}
+                onClick={() => onSort(headCell.id)}
+              >
+                {headCell.label}
+              </TableSortLabel>
+            ) : (
+              headCell.label
+            )}
+          </TableCell>
+        ))}
+        {extraHeadCells?.map((extraHeadCell) => (
+          <TableCell
+            key={extraHeadCell.label}
+            align={extraHeadCell.align}
+            padding={extraHeadCell.padding}
+          >
+            {extraHeadCell.label}
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
 
 export default function BasicTable() {
   const { data } = useGetUsers();
+  const [order, setOrder] = useState<Order>('asc');
+  const [sortedColumn, setsortedColumn] = useState<keyof User>('name');
+
+  const handleSort = (key: keyof User) => {
+    const isAsc = sortedColumn === key && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setsortedColumn(key);
+  };
+
+  const extraColumns: readonly ExtraHeadCell[] = [
+    {
+      label: 'Notes',
+      padding: 'none',
+      align: 'center',
+    },
+    {
+      label: 'Actions',
+      padding: 'none',
+      align: 'center',
+    },
+  ];
 
   return (
     <TableContainer component={Paper}>
       <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell padding="checkbox">
-              <Checkbox color="primary" />
-            </TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Username</TableCell>
-            <TableCell>Email</TableCell>
-            <TableCell>Phone</TableCell>
-            <TableCell align="center">Notes</TableCell>
-            <TableCell align="center">Actions</TableCell>
-          </TableRow>
-        </TableHead>
+        <EnhancedTableHead
+          order={order}
+          orderBy={sortedColumn}
+          onSort={handleSort}
+          extraHeadCells={extraColumns}
+        />
         <TableBody>
           {data ? (
             data.map((row) => (
